45d5f8efb9e50543d7bdc15fc24ae2a2ba1561b5
backend <simhasatya970@gmail.com>
2024-03-14 10:11:09 +0000

    added nodejs

---
+++ my-nodejs-app/App.js
@@ -0,0 +1,17 @@
+app.js
+
+// app.js
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hey Simha Here is Your Nodejs Application!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
